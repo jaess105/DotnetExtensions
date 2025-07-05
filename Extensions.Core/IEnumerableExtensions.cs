72990984b574c159e6783dab87841b2564517b25
jaess105 <jaess105@uni-duesseldorf.de>
2025-07-05 14:25:44 +0200

    Add IsNullOrEmpty extension method

--- Extensions.Core/IEnumerableExtensions.cs
+++ Extensions.Core/IEnumerableExtensions.cs
@@ -13,4 +13,21 @@ public static class IEnumerableExtensions
     /// </returns>
     public static IEnumerable<(int Index, T Element)> Enumerate<T>(this IEnumerable<T> self)
         => self.Select((el, i) => (i, el));
+
+    /// <summary>
+    /// Determines whether an <see cref="IEnumerable{T}"/> is <c>null</c> or contains no elements.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the sequence.</typeparam>
+    /// <param name="enumerable">The sequence to check.</param>
+    /// <returns><c>true</c> if the sequence is <c>null</c> or empty; otherwise, <c>false</c>.</returns>
+    public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this IEnumerable<T>? enumerable)
+        => enumerable is null || !enumerable.Any();
+
+    /// <summary>
+    /// Determines whether a <see cref="string"/> is <c>null</c>, empty, or consists only of white-space characters.
+    /// </summary>
+    /// <param name="str">The string to check.</param>
+    /// <returns><c>true</c> if the string is <c>null</c>, empty, or white-space; otherwise, <c>false</c>.</returns>
+    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? str)
+        => str is null || string.IsNullOrWhiteSpace(str);
 }

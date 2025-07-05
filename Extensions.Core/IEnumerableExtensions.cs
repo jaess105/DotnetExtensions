5129ab46e894a832419a80f954e06c4e0a5bb6e4
jaess105 <jaess105@uni-duesseldorf.de>
2025-07-05 14:07:56 +0200

    rename the named tuple fields

--- Extensions.Core/IEnumerableExtensions.cs
+++ Extensions.Core/IEnumerableExtensions.cs
@@ -3,6 +3,6 @@ namespace Extensions.Core;
 // ReSharper disable once InconsistentNaming
 public static class IEnumerableExtensions
 {
-    public static IEnumerable<(int i, T el)> Enumerate<T>(this IEnumerable<T> self)
+    public static IEnumerable<(int Index, T Element)> Enumerate<T>(this IEnumerable<T> self)
         => self.Select((el, i) => (i, el));
 }
